6b56c61e2cee56c6c5de86909d5c0181621bae26
IntelSam <samarth@intelisync.ai>
2025-04-02 18:05:09 +0530

    Blogs SEO

--- src/app/blogpage/Comprehensive_Guide_Blog/page.js
+++ src/app/blogpage/Comprehensive_Guide_Blog/page.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import Comprehensive_Guide_Blog from "@/component/Blogs_Conatin/Comprehensive_Guide_Blog/Comprehensive_Guide_Blog";
 
+export const metadata = {
+  title: "Crypto Fundraising and Marketing Strategies for Web3 Success",
+  description:
+    "Learn how to raise funds and market your crypto project with ICOs, IDOs, and strategic marketing. Master Crypto Fundraising and Marketing to attract investors.",
+  keywords: "Crypto Fundraising and Marketing",
+  openGraph: {
+    title: "Crypto Fundraising and Marketing Strategies for Web3 Success",
+    description:
+      "Learn how to raise funds and market your crypto project with ICOs, IDOs, and strategic marketing. Master Crypto Fundraising and Marketing to attract investors.",
+    url: "https://intelisync.ai/blogpage/Comprehensive_Guide_Blog/",
+  },
+  alternates: {
+    canonical: "https://intelisync.ai/blogpage/Comprehensive_Guide_Blog/",
+  },
+}
+
 function page() {
   return (
     <div>
